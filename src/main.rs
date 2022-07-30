374d195dff03a37bfb794cf7930c548411412582
arkk200 <arkk20000@gmail.com>
2022-07-31 01:18:41 +0900

    018 ERK: mutable references

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
-// OWNERSHIP - 소유권
-
-/*
-fn return_string() -> &String { // 이 함수는 존재할 수 없다.
-    let country = String::from("대한민국2");
-    // country 변수는 이 함수 내에서만 쓰이는데
-    //이 함수를 참조한 데이터를 보낸다해도
-    // 함수가 끝나면 참조 당했던 변수(country)가
-    // 사라지기에 안전하지 않기 때문이다.
-    return &country
-}
- */
-
-// & = reference
+// &: immutable reference / shared reference
+// &mut는 값을 참조하는데 그 참조한 값을 바꿀 수가 있다.
+/* 하나의 변수를 참조하는 변수가
+여러 개일 경우 위험할 수 있기에
+하나만 쓰는게 안전하다. */
+// &mut: mutable reference / unique refernce
 
 fn main() {
-    let country = String::from("대한민국");
-    let _ref_one = &country;
-    let _ref_two = &country; // 하나의 데이터에 여러개의 참조를 해도 된다.
-
-    println!("Country is: {}", _ref_one);
-    // let _my_country = return_string(); // 안전하지 않다
+    // &mut로 참조당한 변수의 값을 바꿔야 하므로
+    // 참조 당하는 변수도 mut를 써주어야 한다.
+    let mut my_number = 9;
+    // num_ref가 my_number의 값을 바꿀 수 있음
+    let num_ref = &mut &mut my_number;
+    **num_ref = 10;
+    // my_number이 10으로 바뀜
+    println!("Changed number by num_ref is {}", my_number);
+    // mutable reference와 immutable reference는 같이 쓰면 안전하지 않기에
+    // 같이 쓸 수 없다.
 }
