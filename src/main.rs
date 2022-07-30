ba55d9564b3b518cb525f2636276615ad00dd34f
arkk200 <arkk20000@gmail.com>
2022-07-30 22:47:04 +0900

    014 ERK: String and &str

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,33 @@
 fn main() {
-    print!("this"); // 줄바꿈을 안 만듦
-    print!("this2");
-    println!("line_this3"); // print!("line_this3\n")
-    print!("this4");
-    println!("\n-------------------------");
-    print!("this"); // 줄바꿈을 안 만듦
-    print!("this2");
-    print!("line_this3\n");
-    print!("this4");
-    println!("\n-------------------------");
-    // raw text: \n, \t같은 것도 그대로 출력함, r#"문자열"# 형식으로 씀
-    println!(r#"c:\this_drive\new_drive"#);
-    println!("\n-------------------------");
-    // 러스트에선 문자열에 줄바꿈을 해주면 실제로 줄바꿈을 해서 출력해준다.
-    println!(
-"Test
-Teest
-Teeest"
-    );
-    let my_variable = &"9";
-    println!("{:p}", my_variable); // :p는 메모리 주소를 출력해준다.
-    let my_variable = &185;
-    println!("{:o}", *my_variable); // :p는 메모리 주소를 출력해준다.
-    /*
-    :?, 디버그 출력
-    :p, 메모리 주소 출력
-    :x, :X, 16진수로 출력
-    :b, 이진수 출력
-    :o, 팔진수 출력
+    /* String = Sized type
+    크기를 마음대로 늘리거나 줄일 수 있음
+    참조를 하지 않아 자기의 데이터를 가지고 있음
+    변수가 없어지면 데이터가 없어짐
+    관리하기 편함
+    데이터가 힙 영역에 있음 -> 컴파일러가 데이터가 얼마나 큰지 알 수 있음
     */
-    let title = "TODAY'S NEWS";
-    // 중앙(^)으로 정렬, 30글자로 맞추고 양 옆을 ㅁ 으로 채움
-    println!("{:ㅁ^30}", title);
-    // 오른쪽(>)으로 정렬, 15글자로 맞추고 왼쪽을 띄어쓰기로 채움
-    println!("{: >15}", title);
-    // 왼쪽(<)으로 정렬, 20글자로 맞추고 오른쪽을 샍 으로 채움
-    print!("{variable1:샍<20}", variable1 = title) // 마지막 코드는 끝에 세미콜론(;)을 넣을 필요없음
+    /* &str
+    참조를 함, 자기 데이터를 갖지 않음, 크기를 마음대로 조절 못함
+    참조를 하기에 참조를 하는 데이터가 사라지면 안전하지가 않음
+    그렇기에 참조하고 있는 데이터가 삭제되는 것 까지 고려해야함
+    편리하지 않음
+    &을 쓰는 까닭은 그냥 str은 문자열의 크기를 알 수 없다.
+    따라서 참조를 통해 값을 가져온다.
+    */
+    let mut _str1 = "&str"; // 그러나 문자열의 기본 타입은 &str이다
+    /*
+    _str1.pop();
+    _str1.push('!');
+    &str 타입은 위 방법들을 쓸 수 없다.
+    */
+    let _str2 = "String1".to_string(); // String
+    /* .to_string() 을 쓰면 String이 된다.
+    이 외에도 다양한 방법이 있다.
+    */
+    let _str3 = String::from("String2"); // String
+    // growable + shrinkable이 가능하다.
+    let mut my_other_name = "String3".to_string(); // String
+    my_other_name.pop();
+    my_other_name.push('!');
+    println!("{}", my_other_name);
 }
