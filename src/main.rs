ede1656ed1066eead1059e7c0adc57ef7327212b
arkk200 <arkk20000@gmail.com>
2022-07-31 01:46:45 +0900

    020 ERK: references in functions

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,33 @@
+// OWNERSHIP
+/* (1)-- move semantics 
+    함수에서 인자를 받을 때 참조로 받은게 아니기에
+    값에 대한 소유권을 가져간다. (변수 country는 값의 소유권을 뺏기게 됨)
+    따라서 print_country() 함수에 한 번 더 country를 인자로 전달하면
+    값이 이동됐다는 에러가 발생한다.
+*/
+
+fn print_country(country_name: String) {
+    println!("My country is {}", country_name);
+}
+fn print_country_with_return(country_name: String) -> String{
+    println!("My another country is {}", country_name);
+    country_name // 이런식으로 값을 다시 반환해 줄 순 있으나 참조를 쓰는게 더 낫다
+}
+
+fn print_country_with_reference(country_name: &String){
+    println!("My third country is {}", country_name);
+}
+
 fn main() {
-    let mut number = 10;
-    /*
-    let number_ref = &number;
-    let number_change = &mut number_ref;
-    *number_change += 10; // immutable 참조와 mutable 참조를 같이 쓰면 에러가 남
-    */
-    // 비슷한 상황인데 에러가 안나는 경우
-    let number_change = &mut number;
-    *number_change += 10;
-    let number_ref = &number;
-    let _number_ref2 = &number;
-    let _number_ref3 = &number;
-    // immutable 참조와 mutable 참조를 같이 쓸 때
-    // immutable 참조를 많이 해도 mutable 참조가 하나라면
-    // 에러가 나지 않는다.
-    println!("{}", number_ref);
-    
-    // shadowing
-    let country = "my country";
-    let country_ref = &country;
-    let country = 8;
-    // 참조하는 변수는 변수를 참조하는 것이 아니고 값을 참조하는 것이다.
-    println!("{}, {}", country_ref, country); // my country, 8
+    let country = "my country".to_string();
+    print_country(country); // --(1)
+    // print_country(country); // 에러가 발생함
+    let mut another_country = "another country".to_string();
+    // 값은 다시 돌려 받음
+    another_country = print_country_with_return(another_country);
+    println!("{}", another_country);
+    let third_country = "third country".to_string();
+    print_country_with_reference(&third_country);
+    print_country_with_reference(&third_country);
+    print_country_with_reference(&third_country);
 }
