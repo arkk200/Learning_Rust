2975d02f2759bc822570a2010992be8663a49821
arkk200 <arkk20000@gmail.com>
2022-07-31 01:32:41 +0900

    019 ERK: references and shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
-// &: immutable reference / shared reference
-// &mut는 값을 참조하는데 그 참조한 값을 바꿀 수가 있다.
-/* 하나의 변수를 참조하는 변수가
-여러 개일 경우 위험할 수 있기에
-하나만 쓰는게 안전하다. */
-// &mut: mutable reference / unique refernce
-
 fn main() {
-    // &mut로 참조당한 변수의 값을 바꿔야 하므로
-    // 참조 당하는 변수도 mut를 써주어야 한다.
-    let mut my_number = 9;
-    // num_ref가 my_number의 값을 바꿀 수 있음
-    let num_ref = &mut &mut my_number;
-    **num_ref = 10;
-    // my_number이 10으로 바뀜
-    println!("Changed number by num_ref is {}", my_number);
-    // mutable reference와 immutable reference는 같이 쓰면 안전하지 않기에
-    // 같이 쓸 수 없다.
+    let mut number = 10;
+    /*
+    let number_ref = &number;
+    let number_change = &mut number_ref;
+    *number_change += 10; // immutable 참조와 mutable 참조를 같이 쓰면 에러가 남
+    */
+    // 비슷한 상황인데 에러가 안나는 경우
+    let number_change = &mut number;
+    *number_change += 10;
+    let number_ref = &number;
+    let _number_ref2 = &number;
+    let _number_ref3 = &number;
+    // immutable 참조와 mutable 참조를 같이 쓸 때
+    // immutable 참조를 많이 해도 mutable 참조가 하나라면
+    // 에러가 나지 않는다.
+    println!("{}", number_ref);
+    
+    // shadowing
+    let country = "my country";
+    let country_ref = &country;
+    let country = 8;
+    // 참조하는 변수는 변수를 참조하는 것이 아니고 값을 참조하는 것이다.
+    println!("{}, {}", country_ref, country); // my country, 8
 }
