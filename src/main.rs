2d8cf6c93af0fffe747acca8109437fb4b35fab7
arkk200 <arkk20000@gmail.com>
2022-07-31 01:05:13 +0900

    017 ERK: returning a reference

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
-// 러스트도 전역변수, 지역변수가 있다.
-// let은 전역변수로 쓸 수 없는 모양이다.
-// const
-const _NUMBER: i32 = 20; // const는 컴파일러가 알아서 타입을 맞추지 않기에 타입을 무조건 써야 한다. 또한 const는 다 대문자로 써주어야 한다.
-// static
-// static도 마찬가지로 타입을 써줘야 하며, 전부 대문자로 써줘야 한다.
-static mut _LOW_SCORE: i32 = 0;
-// static보단 const를 많이 쓴다.
-static mut _CAN_MUTABLE: i32 = 0; // const와 다르게 static은 mut를 같이 쓸 수 있다.
-// 하지만 안전하지 않기에 안 쓰는 것을 권장한다. --(1), 웬만하면 rust에서 unsafe를 안쓰는게 좋다.
+// OWNERSHIP - 소유권
 
-fn print_high_score() {
-    println!("The high score is {}", _NUMBER);
+/*
+fn return_string() -> &String { // 이 함수는 존재할 수 없다.
+    let country = String::from("대한민국2");
+    // country 변수는 이 함수 내에서만 쓰이는데
+    //이 함수를 참조한 데이터를 보낸다해도
+    // 함수가 끝나면 참조 당했던 변수(country)가
+    // 사라지기에 안전하지 않기 때문이다.
+    return &country
 }
+ */
+
+// & = reference
+
 fn main() {
-    let _x = 8; // 'let' binding: i32, let은 컴파일러가 알아서 타입을 맞추기에 타입을 쓸 필요가 없다.
-    print_high_score();
-    let _y = unsafe {_CAN_MUTABLE}; // (1)-- 그렇기에 만약 쓰게 된다면 unsafe{} 안에 변수를 넣어주어야 한다.
+    let country = String::from("대한민국");
+    let _ref_one = &country;
+    let _ref_two = &country; // 하나의 데이터에 여러개의 참조를 해도 된다.
+
+    println!("Country is: {}", _ref_one);
+    // let _my_country = return_string(); // 안전하지 않다
 }
