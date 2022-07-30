cfc5f671ecde7b1a3a4ed9f7fb8c40362be49995
arkk200 <arkk20000@gmail.com>
2022-07-30 23:22:54 +0900

    015 ERK: String methods

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,30 @@
 fn main() {
-    /* String = Sized type
-    크기를 마음대로 늘리거나 줄일 수 있음
-    참조를 하지 않아 자기의 데이터를 가지고 있음
-    변수가 없어지면 데이터가 없어짐
-    관리하기 편함
-    데이터가 힙 영역에 있음 -> 컴파일러가 데이터가 얼마나 큰지 알 수 있음
+    /* String은 재할당(reallocation)을 한다,
+    예를 들어 16바이트를 할당했는데 데이터가 그 크기보다 넘어서면
+    재할당하여 바이트 크기를 두 배로 늘린다.
+    (추측)
+    바이트 크기가 두 배로 느는건 .push()나 .push_str() 함수가 호출된 후
+    문자열의 크기가 기존 크기를 넘어설 때 늘어난다.
+    값이 존재할 때 최소 값은 8바이트이다.
+     */
+    // method = .function // .을 통해서 호출할 수 있는 함수가 메서드이다.
+    /* String methods 종류
+    .capacity()
+    .push()
+    .push_str()
+    .pop()
     */
-    /* &str
-    참조를 함, 자기 데이터를 갖지 않음, 크기를 마음대로 조절 못함
-    참조를 하기에 참조를 하는 데이터가 사라지면 안전하지가 않음
-    그렇기에 참조하고 있는 데이터가 삭제되는 것 까지 고려해야함
-    편리하지 않음
-    &을 쓰는 까닭은 그냥 str은 문자열의 크기를 알 수 없다.
-    따라서 참조를 통해 값을 가져온다.
-    */
-    let mut _str1 = "&str"; // 그러나 문자열의 기본 타입은 &str이다
-    /*
-    _str1.pop();
-    _str1.push('!');
-    &str 타입은 위 방법들을 쓸 수 없다.
-    */
-    let _str2 = "String1".to_string(); // String
-    /* .to_string() 을 쓰면 String이 된다.
-    이 외에도 다양한 방법이 있다.
-    */
-    let _str3 = String::from("String2"); // String
-    // growable + shrinkable이 가능하다.
-    let mut my_other_name = "String3".to_string(); // String
-    my_other_name.pop();
-    my_other_name.push('!');
-    println!("{}", my_other_name);
+    let mut _my_name = "".to_string();
+    // .len() 값은 .capacity() 값을 초과할 수 없다.
+    // 할당할 바이트 크기를 알면 처음부터 24바이트를 할당하면 재할당이 없다.
+    // let mut _my_name = String::with_capacity(24);
+    println!("Length is {} Capacity is: {}", _my_name.len(), _my_name.capacity()); // 
+    _my_name.push_str("lmj");
+    _my_name.push('!'); // 문자 추가
+    println!("Length is {} Capacity is: {}", _my_name.len(), _my_name.capacity()); // lmj!
+    _my_name.push_str(" and "); // 문자열 추가
+    _my_name.push_str("I live in Seoul!!!!!!!");
+    // _my_name.push('2'); // 두 배로 늘어남
+    println!("Length is {} Capacity is: {}", _my_name.len(), _my_name.capacity()); // lmj! and I ...
+    println!("My na is {}", _my_name);
 }
