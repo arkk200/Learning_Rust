af98ce28dc30b812f3a1ca97fa4a2c3c4ed9730a
arkk200 <arkk20000@gmail.com>
2022-07-31 19:08:22 +0900

    023 ERK: uninitialized variables and for loop

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
-// copy types
-// copy 타입은 값을 복사하기만 하기에 값에 대한 소유권을 빼앗지 않는다.
-// copy 타입을 많이 만들고 복사를 한다고 해도 메모리 양이 줄거나 하진 않는다.
-fn prints_number(number: i32){
-    println!("{}", number);
+fn loop_then_return(mut counter: i32) -> i32 {
+    loop {
+        counter += 1;
+        if counter % 50 == 0{
+            break;
+        }
+    }
+    counter
 }
 
-// String은 copy 타입이 아니다
-// copy 타입 - 하나의 값을 변수들이 봐라봄
-// clone 타입 - 값을 새로 메모리에 할당함
-// clone도 마찬가지로 copy처럼 소유권을 빼앗기지 않는다.
-// 그러나 값을 대하는 방식에 차이가 있다.
-fn prints_string(input: String) {
-    println!("{}", input);
-}
+// uninitialized variable 초기화 되지 않은 변수
+// control flow
 
 fn main() {
-    let my_number = 8;
-    prints_number(my_number);
-    prints_number(my_number);
+    let _number = 8; // 변수는 마지막 코드일지라도 세미콜콘을 써주어야 한다.
+    let _my_second_num: u16; // 초기화 되지 않은 변수, 값이 없어서 쓸 수가 없음
+    // 블럭 안에서 값을 수정해도 변수가 선언된 위치를 변수의 활동 범위로 잡음
+    { _my_second_num = 10; } // 전에 말했듯이 최초 1회까지 값을 할당해주기만 하면 된다.
+    println!("{}", _my_second_num);
 
-    let my_country = "Korea".to_string();
-    prints_string(my_country.clone());
-    prints_string(my_country);
+    let _my_variable = {
+        let x = loop_then_return(43);
+        x
+    };
+    println!("{}", _my_variable);
 }
