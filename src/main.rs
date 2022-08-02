316b9abd6bb97c6c4108a1d34dc658caf5dbeb34
arkk200 <arkk20000@gmail.com>
2022-08-02 21:39:06 +0900

    032 ERK: Structs

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,45 @@
-fn match_coulours(rgb:(u32, u32, u32)){
-    match rgb {
-        // 특정 값만 볼 경우 안 볼 값들은 _ 처리 해준다.
-        (r, _, _) if r < 10 => println!("Not much red"),
-        (_, g, _) if g < 10 => println!("Not much green"),
-        (_, _, b) if b < 10 => println!("Not much blue"),
+// 러스트에는 struct가 3가지가 있다.
+// unit struct - 아무것도 없는, 이름만 있는 struct
+// unit struct는 바이트 크기가 0이다.
+struct FileDirectory; // struct의 앞 글자는 무조건 대문자이다.
 
-        // rgb 다 10 이상일 경우
-        _ => println!("Every colour has at least 10")
-    }
+fn take_file_directory(_input: FileDirectory) { // 아무것도 없어도 쓸 수 있다.
+    println!("I got a file directory");
 }
 
-fn match_number(input: i32) {
-    match input {
+// tuple struct
+// #[derive(Debug)]
+struct Colour (u8, u8, u8);
 
-        // _와 마찬가지로 조건이 없으므로 이 코드밖에 실행이 안됨
-        // number => println!("It's the number {}", number),
-
-        // 슬라이스 때 썼던 걸 쓸 수 있음
-        // 이 때 만약 값을 가져오고 싶으면 앞에 '변수명 @' 를 쓰면 된다.
-        number @ 0..=10 => println!("It's between 0 and 10. It's the number {} ", number),
-        _ => println!("It's greater than ten")
-    }
+// name struct
+#[derive(Debug)]
+struct Country {
+    population: u32,
+    capital: String,
+    leader_name: String
 }
 
 fn main() {
-    let first = (200, 0, 0);
-    let second = (50, 50, 50);
-    let third = (200, 50, 0);
-
-    match_coulours(first); // first같은 경우 match의 특성에 의해서 Not much gree만 출력된다.
-    match_coulours(second);
-    match_coulours(third);
-
-    /*
-    let 변수 = match할 때 match에서 반환하는 값들의 타입은 다 같아야 한다.
-    */
-    // let i = match 1 { 3 => 1, 1 => "1", _ => '샍' } // 이렇게 하면 에러가 발생함
-    
-    // 다른 예
-    // 얘도 주는 타입이 다르므로 에러가 발생함
-    // let j = if 10 == 9 { 8 } else { "Something else" }
+    // unit struct (거의 안 씀)
+    let x = FileDirectory;
+    println!("The size is {}", std::mem::size_of_val(&x));
+    take_file_directory(x);
 
     println!("");
 
-    match_number(10);
-    match_number(100);
+    // tuple struct
+    let my_colour = Colour(20, 50, 100);
+    println!("The second colour is {}", my_colour.1); // 튜플은 프로퍼티를 이용해서 요소를 가져옴
+    // println!("The second colour is {:?}", my_colour);
+
+    println!("");
+
+    // name struct
+    let canada = Country {
+        population: 35_000_000,
+        capital: "Ottawa".to_string(),
+        leader_name: "Justin Trudeau".to_string()
+    };
+    println!("The population is: {}\nThe capital is: {}\nLeader name is: {}", canada.population, canada.capital, canada.leader_name);
+    println!("The country is: {:#?}", canada); // Debug print에 #을 사이에 넣으면 깔끔하게 출력됨
 }
