9b034c5a75166994580a417974531054f3285661
arkk200 <arkk20000@gmail.com>
2022-08-03 00:29:13 +0900

    039 ERK: impl blocks

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,39 @@
-// loops
+// impl blocks (implement blocks)
+
+#[derive(Debug)]
+struct Animal {
+    age: u8,
+    animal_type: AnimalType
+}
+
+#[derive(Debug)]
+enum AnimalType {
+    Cat,
+    Dog
+}
+
+impl Animal {
+    // new는 러스트에서 키워드가 아님 써도 상관없음
+    // Self == Animal, Self는 struct Animal을 가르킴
+    fn new() -> Self { // function signature
+        Self {
+            age: 10,
+            animal_type: AnimalType::Cat
+        }
+    }
+    fn new_cat(age:u8) -> Self { // function signature
+        Self {
+            age,
+            animal_type: AnimalType::Cat
+        }
+    }
+}
 
 fn main() {
-    let mut counter = 0;
-
-    // loop는 러스트와 맞지 않기에 while을 쓰는게 낫다
-    while counter != 5 { // '루프명: loop 로 이름을 지정해줄 수 있다.
-        counter += 1;
-        println!("The counter is: {}", counter);
-    }
-
-    // ---------------
-
-    // Range: 러스트에서 range는 n..m 식으로 쓴다.
-    // exclusive Range: n..m
-    // inclusive Range: n..=m
-    // 값을 받는 변수도 앞에 _를 붙여서 컴파일러가 무시할 수 있게 할 수 있다.
-    for number in 0..3 { // exclusive Range
-        println!("The number is {}", number);
-    }
-
-    // ---------------
-
-    let mut counter = 5;
-
-    let my_number = loop {
-        counter += 1;
-        if counter % 53 == 3{
-            break counter; // break하면서 counter를 return함
-        }
-    };
-    println!("my_number is now: {}", my_number);
+    // new()를 호출하면 impl에 있는 fn new의 Self가 반환됨
+    // associated function, 타입하고 관련된 함수이며 아직 메서드가 안됨
+    let my_animal = Animal::new();
+    println!("{:?}", my_animal);
+    let my_animal = Animal::new_cat(5);
+    println!("{:?}", my_animal);
 }
