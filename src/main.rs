50f7c36ac98d11707ad0d713e58453b8035f3f5d
arkk200 <arkk20000@gmail.com>
2022-08-02 23:59:32 +0900

    038 ERK: More loops

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,32 @@
 
 fn main() {
     let mut counter = 0;
-    let mut counter2 = 0;
 
-    'first_loop: loop { // '루프명: loop 로 이름을 지정해줄 수 있다.
+    // loop는 러스트와 맞지 않기에 while을 쓰는게 낫다
+    while counter != 5 { // '루프명: loop 로 이름을 지정해줄 수 있다.
         counter += 1;
         println!("The counter is: {}", counter);
-        if counter > 9 {
-            println!("Now entering the second loop");
-
-            'second_loop: loop {
-                println!("The seconde counter is: {}", counter2);
-                counter2 += 1;
-                if counter2 == 3 {
-                    break 'first_loop; // break 옆에 지정한 루프명을 써서 원하는 루프를 break 시킬 수 있다.
-                }
-            }
-        }
     }
+
+    // ---------------
+
+    // Range: 러스트에서 range는 n..m 식으로 쓴다.
+    // exclusive Range: n..m
+    // inclusive Range: n..=m
+    // 값을 받는 변수도 앞에 _를 붙여서 컴파일러가 무시할 수 있게 할 수 있다.
+    for number in 0..3 { // exclusive Range
+        println!("The number is {}", number);
+    }
+
+    // ---------------
+
+    let mut counter = 5;
+
+    let my_number = loop {
+        counter += 1;
+        if counter % 53 == 3{
+            break counter; // break하면서 counter를 return함
+        }
+    };
+    println!("my_number is now: {}", my_number);
 }
