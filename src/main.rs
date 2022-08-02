ecea6300e0aa2c6663bba7525ab9aa8810d2a941
arkk200 <arkk20000@gmail.com>
2022-08-02 22:45:26 +0900

    035 ERK: Enums 2

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,50 @@
-// struct = and
-// enum = or (선택)
+// use: 함수 같은걸 줄여줌
 
-// enum은 enumeration을 줄인말이다.
-// e = from, number
-
-enum ThingsInTheSky { // enum도 struct처럼 대문자로 시작한다.
-    Sun,
-    Starts,
+enum Mood {
+    Happy,
+    Sleepy,
+    NotBad,
+    Angry
 }
 
-fn create_skystate(time: i32) -> ThingsInTheSky {
-    match time {
-        // enum의 요소 접근은 'ENUM::요소' 형식으로 쓰면 된다.
-        6..=18 => ThingsInTheSky::Sun,
-        _ => ThingsInTheSky::Starts
+fn match_mood(mood: &Mood) -> i32 {
+    use Mood::*; // Mood::*;을 쓰면
+    // Mood::Happy, Mood::Sleepy... 앞에 'Mood::'를 제거할 수 있음
+
+    // match앞에 변수넣고 그 변수를 굳이 반환할 필요없이
+    // match에서 return되는 값을 바로 return할 수 있다.
+    match mood {
+        Happy => 10,
+        Sleepy => 6,
+        NotBad => 7,
+        Angry => 2
     }
 }
 
-fn check_skystate(state: &ThingsInTheSky) {
-    match state {
-        ThingsInTheSky::Sun => println!("I can see the sun"),
-        ThingsInTheSky::Starts => println!("I can see the starts")
-    }
+// -----------
+
+enum Season {
+    // enum은 컴파일러가 볼 때 숫자가 있다.
+    Spring, // 0
+    Summer, // 1
+    Autumn, // 2
+    Winter, // 3
 }
 
 fn main() {
-    let time = 20;
-    let sky_state = create_skystate(time);
-    check_skystate(&sky_state);
+    let my_mood = Mood::NotBad;
+    let happiness_level = match_mood(&my_mood);
+    println!("Out of 1 to 10, my happiness is {}", happiness_level);
 
-    // 한 번에 한 줄로 줄임
-    check_skystate(&create_skystate(8));
+    // -----------
+    use Season::*;
+
+    // Vec<Season> 타입
+    let four_seasons = vec![Spring, Summer, Autumn, Winter];
+
+    for season in four_seasons {
+
+        // as 정수로 하면 컴파일러가 인식하는 숫자가 출력된다.
+        println!("The number is: {}", season as u32);
+    }
 }
