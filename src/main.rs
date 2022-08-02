710227a96c297c57452b971b84d003f6b9bc0ab5
arkk200 <arkk20000@gmail.com>
2022-08-03 02:09:04 +0900

    040 ERK: More impl blocks

--- src/main.rs
+++ src/main.rs
@@ -12,28 +12,50 @@ enum AnimalType {
     Dog
 }
 
-impl Animal {
-    // new는 러스트에서 키워드가 아님 써도 상관없음
-    // Self == Animal, Self는 struct Animal을 가르킴
-    fn new() -> Self { // function signature
-        Self {
-            age: 10,
-            animal_type: AnimalType::Cat
-        }
+impl Animal { // 같은 이름의 impl을 더 만들어도 된다.
+    fn new_old_cat() -> Self {
+        Self { age: 15, animal_type: AnimalType::Cat }
     }
+}
+
+impl Animal {
     fn new_cat(age:u8) -> Self { // function signature
         Self {
             age,
             animal_type: AnimalType::Cat
         }
     }
+    fn new_dog(age:u8) -> Self { // function signature
+        Self {
+            age,
+            animal_type: AnimalType::Dog
+        }
+    }
+
+    fn print(&self) {
+        println!("I am a: {:?}", self);
+    }
+
+    fn change_to_dog(&mut self){
+        self.animal_type = AnimalType::Dog;
+        println!("Changed to dog! Now I am: {:?}", self);
+    }
+    fn change_to_cat(&mut self){
+        self.animal_type = AnimalType::Cat;
+        println!("Changed to cat! Now I am: {:?}", self);
+    }
 }
 
 fn main() {
     // new()를 호출하면 impl에 있는 fn new의 Self가 반환됨
     // associated function, 타입하고 관련된 함수이며 아직 메서드가 안됨
-    let my_animal = Animal::new();
-    println!("{:?}", my_animal);
-    let my_animal = Animal::new_cat(5);
-    println!("{:?}", my_animal);
+    let mut my_animal = Animal::new_dog(5);
+    my_animal.print(); // syntactic sugar
+    my_animal.change_to_cat();
+    my_animal.change_to_dog();
+    // 아래 방법을 쓰기 귀찮으니깐 위와 같은 syntactic sugar 방식을 쓴다.
+    Animal::print(&my_animal);
+
+    let my_old_cat = Animal::new_old_cat();
+    println!("{:?}", my_old_cat);
 }
