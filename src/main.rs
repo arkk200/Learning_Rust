229e2c82081be1ec5a25a2944caf3581d4c882ba
arkk200 <arkk20000@gmail.com>
2022-08-02 19:57:21 +0900

    028 ERK: Tuples and destructuring

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
-// trait
+fn main() /*-> ()*/ { // main함수에선 안보이는 튜플을 반환하고 있다.
+    let my_tuple = (8, "lmj", vec![1, 2, 3]); // tuple은 소괄호만 써서 만들 수 있다.
+    println!("{:?}", my_tuple); // tuple은 다 똑같은 타입일 필요가 없다.
+    // tuple내에 요소의 타입을 알고 싶으로 뒤에 아무글자의 메소드를 호출하면 된다.
+    // my_tuple.wefuhwaeogv();
+    let random_tuple = ("Here is a name", 8, vec!['a'], 'b', [8, 9, 10], 7.7);
+    println!(
+        "Inside the tuple is: First item: {:?}
+Seconde item: {}
+Third item: {:?}
+Fourth item: {:?}
+Fifth item: {:?}
+Sixth item: {}", // 정수, 실수는 디버그 프린트를 쓸 필요가 없다.
+        random_tuple.0, // 튜플이 Vec, Array와 다른 점은 요소를 가져올 때 프로퍼티를 이용해서 가져온다.
+        random_tuple.1,
+        random_tuple.2,
+        random_tuple.3,
+        random_tuple.4,
+        random_tuple.5,
+    );
 
-// From, Into
+    // Vec에 두 개의 자료형을 넣게하고 싶으면 tuple을 쓰면 된다
+    // Vec<(String, i32)>
+    let _my_vec = vec![("Hey", 9), ("Hello there", 321241)];
 
-fn main() {
-    // from이 있으면 into도 쓸 수 있음
-    let my_name = String::from("lmj");
-    let my_city:String = "Busan".into();
-
-    println!("{} {}", my_name, my_city);
-    // String도 마찬가지로 변수명.아무글잠ㅈㄷㄻㅈ() 식으로 치면 타입을 볼 수 있음
-    // my_city.aweiuvbwscewviabwe();
-    let _my_vec: Vec<i32> = Vec::from([8, 9, 10]); // Vec은 from trait가 있음
+    // Destructuring
+    let str_tuple = ("one", "two", "three");
+    let (a, b, c) = str_tuple; // 러스트에도 비구조화 할당을 할 수 있다.
+    let (d, _, _) = str_tuple; // _는 할당을 안 받을 경우 쓴다. 또한 요소 개수만큼 변수를 개수를 맞춰 줘야한다.
+    println!("{}, {}, {}, {}", a, b, c, d);
 }
