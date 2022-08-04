cb7ee8add3d9df790897f876cdf30431f0eb3728
arkk200 <arkk20000@gmail.com>
2022-08-04 23:17:59 +0900

    050 ERK: if let and while let

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,55 @@
-use std::num::ParseIntError;
-
-// 5가 나오면 Ok, 아니면 Err가 나오는& 함수
-fn check_if_five(number: i32) -> Result<i32, String> {
-    match number {
-        5 => Ok(number),
-        _ => Err("Error!! It's not five".to_string())
-    }
-}
-
-// Result<T, E>타입을 반환할 땐 Ok(T), Err(E)를 반환해야한다.
-fn parse_number(num: &str) -> Result<i32, ParseIntError> {
-    // 숫자만 있는 문자열이 들어가야 에러가 안 남
-    num.parse() // parse는 자기만의 에러를 가지고 있음: ParseIntError
-}
-
 fn main() {
-    // Vec<Result<i32, String>>
-    let mut result_vec = Vec::new();
+    let my_vec = vec![2, 3, 4];
+    // .get은 배열에서 해당 인덱스에 값이 있으면 Some(값),
+    // 없으면 None을 반환함
+    let _get_one = my_vec.get(0);
+    let _get_two = my_vec.get(20);
+    println!("{:?}, {:?}", _get_one, _get_two);
 
-    for number in 2..=7 {
-        result_vec.push(check_if_five(number));
+    // 이를 활용해서 안전하게 원하는 값만 출력할 수 있다.
+    for index in 0..10 {
+        match my_vec.get(index) {
+            Some(num) => println!("The number is: {}", num),
+            None => {}
+        }
+
+        // if let을 이용하면
+        /*
+        let a = Some(0i32);
+        match a {
+            Some(3) => println!("three"),
+            _ => (),
+        } 를
+        if let Some(3) = a {
+            println!("three");
+        } 로 줄일 수 있다. (비교를 할 때 = 을 하나만 씀)
+        출처: https://rinthel.github.io/rust-lang-book-ko/ch06-03-if-let.html
+        if let은 서로 패턴이 대응된다면 패턴식을 수행한 후 분기로 접어든다.
+        */
+        if let Some(num) = my_vec.get(index) {
+            println!("The number is: {}", num);
+        }
     }
+    
+    // if let과 마찬가지로 while let도 존재한다, while let도 마찬가지이다.
+    
+    let weather_vec = vec![
+        vec!["Berlin", "cloudy", "5", "-7", "78"],
+        vec!["Athens", "sunny", "not humid", "20", "10", "50"]
+    ];
 
-    println!("{:?}", result_vec);
+    println!("");
 
-    // parse - 숫자로 만들어줌
-
-    let mut my_vec = vec![];
-    my_vec.push(parse_number("8"));
-    my_vec.push(parse_number("aweofi")); // 에러가 남
-    my_vec.push(parse_number("10"));
-
-    for num in my_vec{
-        println!("{:?}", num);
+    for mut city in weather_vec {
+        println!("For the city of: {}", city[0]);
+        // vec!["Berlin", "cloudy", ...]에서 하나씩 pop()을 하고
+        // 안에 있던 문자열을 반환함 (pop()은 뒤에서부터 하나씩 요소를 제거함)
+        while let Some(information) = city.pop() {
+            // information.parse를 할 때 타입이 i32인게 나오면
+            // Ok(_)의 패턴식과 같아지고 패턴식을 실행함
+            if let Ok(number) = information.parse::<i32>() { // turbofish ::<>
+                println!("The number is: {}", number);
+            }
+        }
     }
 }
