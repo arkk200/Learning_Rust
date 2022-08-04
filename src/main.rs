8a2cfd520a7f016bfbf80a0bdf0426e683c8b75f
arkk200 <arkk20000@gmail.com>
2022-08-05 06:43:54 +0900

    054 ERK: HashMap entry and HashSet

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,58 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 fn main() {
-    let canadian_cities = vec!["Calgary", "Vancouver", "Gimli"];
-    let german_cities = vec!["Karlsruhe", "Bad Doberan", "Bielefeld"];
+    let data = vec![
+        ("female", 9),
+        ("male", 5),
+        ("male", 0),
+        ("female", 6),
+        ("female", 5),
+        ("male", 10),
+    ];
 
-    let mut city_hashmap = HashMap::new();
+    let mut survey_hashmap = HashMap::new();
 
-    for city in canadian_cities {
-        // HashMap은 key, value 둘 다 있어야 한다.
-        city_hashmap.insert(city, "Canada");
+    for (gender, num) in data { // (&strm i32)
+        // 튜플로 이루어진 배열이므로 프로퍼티로 가져옴
+        // 만약 해당 키가 없으면 Vec을 value로 만들고 push를 함
+        // 있을 경우 Vec은 value로 다시 만들지 않고
+        // 이미 만들어진 Vec에 push만 함
+        survey_hashmap.entry(gender).or_insert(Vec::new()).push(num);
     }
 
-    for city in german_cities {
-        city_hashmap.insert(city, "Germany");
+    for (male_or_femail, nums) in survey_hashmap {
+        println!("{:?}, {:?}", male_or_femail, nums);
     }
 
-    println!("{:?}", city_hashmap["Bielefeld"]);
 
-    // 좀 더 안전하게 하려면 HashMap 역시 .get()을 쓰면된다.
-    println!("{:?}", city_hashmap.get("Bielefeld"));
-    println!("{:?}", city_hashmap.get("Doesn't include this"));
+    // HashSet은 어떤 배열에 값이 있는지 없는지만 확인할 때 쓰면 된다.
+    let many_numbers = vec![
+        94, 42, 59, 64, 32, 22, 38, 5, 59, 49, 15, 89, 74, 29, 14, 68, 82, 80, 56, 41, 36, 81, 66,
+        51, 58, 34, 59, 44, 19, 93, 28, 33, 18, 46, 61, 76, 14, 87, 84, 73, 71, 29, 94, 10, 35, 20,
+        35, 80, 8, 43, 79, 25, 60, 26, 11, 37, 94, 32, 90, 51, 11, 28, 76, 16, 63, 95, 13, 60, 59,
+        96, 95, 55, 92, 28, 3, 17, 91, 36, 20, 24, 0, 86, 82, 58, 93, 68, 54, 80, 56, 22, 67, 82,
+        58, 64, 80, 16, 61, 57, 14, 11];
 
+    // HashSet은 generic이 하나만 들어간다.
+    let mut number_hashset = HashSet::new();
     
-    let mut book_hashmap = HashMap::new();
+    for num in &many_numbers {
+        number_hashset.insert(num);
+    }
 
-    book_hashmap.insert(1, "L'Allemagne Noderne");
-    book_hashmap.insert(1, "Le Petit Prince");
-    book_hashmap.insert(1, "섀도우 오브 유어 스마일");
-    book_hashmap.insert(1, "Eye of the World");
-    // 최종적으로 "Eye of the World"가 덮어 씀
+    // HashSet은 이름 그대로 집합으로 중복되는 원소는 하나로 없앤다.
+    let hashset_length = number_hashset.len();
+    println!("Total Length: {}, There are {} unique numbers, so we are missing {}.", &many_numbers.len(), hashset_length, 100 - hashset_length);
 
-    // hashmap에서 키를 가져올 땐 값을 빌려주어야 한다.
-    println!("{:?}", book_hashmap.get(&1));
+    let mut missing_vec = vec![];
+    for number in 0..100 {
+        if number_hashset.get(&number).is_none() {
+            missing_vec.push(number);
+        }
+    }
 
-    if let Some(book_name) = book_hashmap.get(&1) {
-        println!("Already got a book: \"{}\"", book_name);
-    } else {
-        book_hashmap.insert(1, "Le Petit Price");
+    print!("It does not contain: ");
+    for num in missing_vec {
+        print!("{} ", num);
     }
 }
