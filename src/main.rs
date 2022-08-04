8bd340063054d98d8d20c2bd30995ba3ba984003
arkk200 <arkk20000@gmail.com>
2022-08-04 09:19:08 +0900

    044 ERK: Generics

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,40 @@
-// Dereference and the dot operator
+// generics <-> concrete
 
-struct Item {
-    number: u8
+// concrete: i32, String...
+
+// angle bracket(<>)이 있으면 러스트 컴파일러가 generic타입으로 인식함
+
+// 꼭 T일 필요는 없음
+fn give_thing<T>(input: T) -> T { // T
+    input
 }
 
-impl Item {
-    fn compare_number(&self, other_number: u8) {
-        println!("Are they equal? {}", self.number == other_number);
-    }
+fn _another_give_thing<T>(input: T) -> T {
+    // generic이 받은 자료형이 만약 구조체라면
+    // Display, Debug 출력도 안되기에 이 코드는 안전하지 않다
+    // println!("{}", input);
+    input
 }
 
+use std::fmt::Display;
+// 만약 Display로 출력하고 싶다면 generic에 
+// Display trait이 있어야 하기에 std::fmt::Display를 적어준다.
+fn fixed_give_thing<T: Display>(input: T) -> T {
+    println!("{}", input);
+    input
+}
+
+struct _Book;
 fn main() {
-    let my_number = 10;
-    let _reference = &my_number;
+    // generic은 여러가지 자료형을 받을 수 있다.
+    let x = give_thing(String::from("Take this thing"));
+    let y = give_thing(9);
+    println!("{}", x);
+    println!("{}", y);
 
-    // i32와 &i32는 타입이 달라 에러가 남
-    // println!("Are they the same? {}", my_number == _reference);
-
-    let item = Item {
-        number: 10
-    };
-
-    let _reference = &item.number; // &u8
-    // 얘 역시도 에러가 남
-    // println!("{}", _reference == 10);
-
-
-    let reference_item = &item; // &item
-    let other_reference_item = &reference_item; // &&item
-
-    // .연산자를 쓰면 러스트가 알아서 dereferencing 함 (*을 쓸 필여가 없음)
-    reference_item.compare_number(10);
-    other_reference_item.compare_number(10);
+    fixed_give_thing(123);
+    
+    // 그러나 fixed_give_thing은 Display 출력이 가능한 타입만 받기에
+    // struct를 주면 에러가 난다.
+    // fixed_give_thing(Book);
 }
