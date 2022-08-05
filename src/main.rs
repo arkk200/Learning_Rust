73acc257a9fbf72eeab48c67c1c95a8d64e9f5fb
arkk200 <arkk20000@gmail.com>
2022-08-05 10:16:43 +0900

    057 ERK: Question mark operator

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
-use std::collections::VecDeque;
+// Question mark operator
 
-// VecDeque
-// Deque(double - ended queue)는
-// 양방향으로 값을 집어넣을 수 있는 queue를 의미한다.
+use std::num::ParseIntError;
 
-fn main() {
-    // Vec도 .remove(0)을 써서 앞에 값을 빼올 수 있다.
-    let mut _my_vec = vec![8, 9, 10];
-    println!("{}, {:?}", _my_vec.remove(0), _my_vec);
-    // remove는 값을 제거하면 뒤에서부터 값들을 한칸씩 앞으로 당긴다.
-    // 만약 배열 길이가 길어진다면 성능이 떨어진다는 단점이 있기에 이럴 땐 Deque를 사용한다.
-    let mut _very_larget_vec = vec![0; 600_000];
+fn parse_str(input: &str) -> Result<i32, ParseIntError> {
+    // ?는 match를 해주는데 에러가 있으면 error을 반환한다.
+    // 물음표 연산자를 쓸 때 무조건 반환하는게 Result여야한다.
+    // 에러가 있을 경우 에러를 반환하는데 그에 맞는 타입을 써줘야 하기 때문이다.
+    let parsed_number = input.parse::<i32>()?; // return error
+    println!("It worked! {}", parsed_number);
 
-    /*
-    for _ in 0..600_000 { // 60만 번 값을 제거하면서 1800억번정도 값을 복사하고 앞 인덱스에 붙여넣음
-        _very_larget_vec.remove(0);
-    }
-    */
+    // ? 는 에러가 생길 수 있는 위치에 더 써줄 수도 있다.
+    let parsed_number = input.parse::<u16>()?.to_string().parse::<u32>()?.to_string().parse::<i32>()?;
 
-    // VecDeque
-    let mut _very_larget_vecdeque = VecDeque::from(vec![0; 600_000]);
-    for _ in 0..600_000 { // 60만 번 정도 pop()하기만 함
-        // push_front(), pop_front()로 앞에서부터 값을 넣거나 제거하고
-        // push_back(), pop_back()으로 뒤에서부터 값을 넣거나 제거함
-        _very_larget_vecdeque.pop_front();
+    Ok(parsed_number) // Result를 반환하므로 이런식으로 써야한다.
+}
+
+fn main() { // 9.0은 안됨
+    for i in vec!["Seven", "8", "9.0", "nice", "6060"] {
+        let parsed = parse_str(i);
+        println!("{:?}", parsed);
+        // if let Ok(num) = parsed {
+        //     println!("{}", num); // Result를 반환받으므로 Debug print를 씀
+        // }
     }
 }
